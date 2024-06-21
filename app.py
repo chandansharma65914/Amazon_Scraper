44360dce300b51bf8b2b96496f9c8a502e1599a1
Chandan Sharma <109021997+chandansharma65914@users.noreply.github.com>
2024-06-21 08:24:46 +0530

    Add files via upload
    added both fiels

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, jsonify
+from bs4 import BeautifulSoup
+import requests
+import re
+
+app = Flask(__name__)
+
+@app.route('/api/products', methods=['POST'])
+def get_product_details():
+    data = request.get_json()
+    asins = data.get('asins', [])
+    result = {}
+
+    for asin in asins:
+        result[asin] = crawl_by_id(asin)
+
+    return jsonify(result)
+
+def crawl_by_id(id):
+    o = {}
+    target_url = f"https://www.amazon.com/dp/{id}"
+    headers = {
+        "accept-language": "en-US,en;q=0.9",
+        "accept-encoding": "gzip, deflate, br",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
+    }
+    
+    resp = requests.get(target_url, headers=headers)
+
+    if resp.status_code != 200:
+        return {'error': f'Failed to fetch data for ASIN: {id}'}
+
+    soup = BeautifulSoup(resp.text, 'html.parser')
+
+    try:
+        o["title"] = soup.find('h1', {'id': 'title'}).text.lstrip().rstrip()
+    except:
+        o["title"] = None
+
+    images = re.findall('"hiRes":"(.+?)"', resp.text)
+    o["thumbnail"] = images[0] if images else None
+
+    try:
+        o["price"] = soup.find("span", {"class": "a-price"}).find("span").text
+    except:
+        o["price"] = None
+
+    try:
+        o["productDescription"] = soup.find("div", {"id": "productDescription"}).text
+    except:
+        o["productDescription"] = None
+
+    return o
+
